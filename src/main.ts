f9b79f87150f4aeb9fd97eac027bccbc2f7a223f
Amane Katagiri <amane@ama.ne.jp>
2023-09-28 13:50:47 +0900

    Add vanjs

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./css/main.css";
+import van from "./van/van-1.2.1.js";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const { div } = van.tags;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const addApp = (id: string) => {
+  const target = document.querySelector(`#${id}`);
+  if (target != null) {
+    van.add(target, div("cosharet"));
+  }
+};
+
+addApp("app");
