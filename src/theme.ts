c133d2a33c5bada6c27e3d4ca82cb144e493fd66
Amane Katagiri <amane@ama.ne.jp>
2023-10-25 09:44:06 +0900

    Fix color customize engine

--- src/theme.ts
+++ src/theme.ts
@@ -12,7 +12,7 @@ export type Theme<T extends ThemeKey = ThemeKey> = {
 const isColor = (colorLike: unknown) =>
   typeof colorLike === "string" &&
   colorLike.match(
-    /#[0-9a-f]{3}|#[0-9a-f]{4}|#[0-9a-f]{6}|#[0-9a-f]{8}|[a-zA-Z]+|\(rgba?|hsla?|\(repeat-\)?\(linear|conic|radial\)-gradient\)(.+)/,
+    /^(#[0-9a-f]{3}|#[0-9a-f]{4}|#[0-9a-f]{6}|#[0-9a-f]{8}|[a-zA-Z]+|(rgba?|hsla?|(repeat-)?(linear|conic|radial)-gradient)\(.+\))$/,
   );
 
 export const parseColorTheme = (
@@ -23,16 +23,16 @@ export const parseColorTheme = (
     ...fallback,
     ...(isColor(themeLike["text"]) ? { text: themeLike["text"] } : {}),
     ...(isColor(themeLike["background"])
-      ? { text: themeLike["background"] }
+      ? { background: themeLike["background"] }
       : {}),
     ...(isColor(themeLike["componentBackground"])
-      ? { text: themeLike["componentBackground"] }
+      ? { componentBackground: themeLike["componentBackground"] }
       : {}),
     ...(isColor(themeLike["selectedItemBackground"])
-      ? { text: themeLike["selectedItemBackground"] }
+      ? { selectedItemBackground: themeLike["selectedItemBackground"] }
       : {}),
     ...(isColor(themeLike["accentColor"])
-      ? { text: themeLike["accentColor"] }
+      ? { accentColor: themeLike["accentColor"] }
       : {}),
   };
 };
